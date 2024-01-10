7784504891e5c429e19f9e3992a61b6e19f3e682
hanif <h.ehmedov@gmail.com>
2024-01-10 17:04:35 -0500

    port 0.0.0.0

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ app.set('ioserver', IOserver)
 // error Handler
 app.use(errorHandler);
 
-server.listen(PORT, "127.0.0.1", () => {
+server.listen(PORT, () => {
 	console.log(":: Server running on port", PORT);
 });
 
